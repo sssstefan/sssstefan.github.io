1695f01017e2418f39565fa20efb82f905ac8bc4
sssstefan <zhip.y7@gmail.com>
2026-01-21 06:14:37 +0000

    Deploying to gh-pages from @ sssstefan/sssstefan.github.io@ba962c486565bf571c2114867e8b52f1ded060f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,11 +45,11 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=1nVpMXgAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/zhipengyin/", "_blank");
+          window.open("https://www.linkedin.com/in/zhi.y7@gmail.com", "_blank");
         },
       },{
       id: 'light-theme',
